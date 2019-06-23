100c02c766114098d87b988fb9aa2ebb257e55f9
Hugo <hchastel@laposte.net>
2019-06-23 18:58:13 +0200

    WIP: Added a table to display links' status from a given link

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,115 @@
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate prettytable;
+
+extern crate regex;
 extern crate reqwest;
 use std::env;
+
+use regex::Regex;
+use prettytable::{Table, Row, Cell};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("usage : cargo run <url to check>");
+        eprintln!("usage : cargo run <link to check>");
         return;
     }
-    let url_to_check = &args[1];
+    let link_to_check = &args[1];
 
-    println!("checking url : {:?}!", url_to_check);
-    let res = check_url(url_to_check.to_string());
-    println!("result : {:?}!", res);
+    println!("checking link : {:?}!", link_to_check);
+    let res = retrieve_html(link_to_check.to_string());
+    let links = retrieve_links_from_html(&res);
+    println!("link : {:?}!", links);
+
+
+    let status = are_websites_up(&links);
+    let sub_table = get_sub_table(links, status);
+    let table = get_table(link_to_check, !res.is_empty(), sub_table);
+    table.printstd();
 }
 
-fn check_url(url: String) -> bool {
-	let response = reqwest::get(&url).unwrap();
-	response.status().is_success()
+fn retrieve_html(link: String) -> String {
+    let mut response = reqwest::get(&link).unwrap();
+    response.text().unwrap()
 }
+
+fn retrieve_links_from_html(text: &str) -> Vec<&str> {
+    lazy_static! {  // build the regex only once
+        static ref RE: Regex = Regex::new(r#"<a [^>]* href="([^"]+)"([^>]|\s)*>"#).unwrap();
+    }
+
+    let mut res = Vec::new();
+    for link in RE.captures_iter(&text) {
+        res.push(link.get(1).unwrap().as_str());
+    }
+    res
+}
+
+fn is_website_up(link: &str) -> bool {
+    let resp = reqwest::get(link);
+
+    match resp {
+        Err(e) => {
+            println!("err");
+            false
+        },
+        Ok(r) => {
+            if r.status().is_success() {
+                println!("success!");
+                true
+            } else if r.status().is_server_error() {
+                println!("server error!");
+                false
+            } else {
+                println!("Something else happened. Status: {:?}", r.status());
+                false
+            }
+        }
+    }
+}
+
+fn are_websites_up(links: &Vec<&str>) -> Vec<bool> {    // TODO create Client connection for reuse
+    let mut res = Vec::new();
+    for link in links {
+        res.push(is_website_up(link));
+    }
+    res
+}
+
+fn get_sub_table(links: Vec<&str>, status: Vec<bool>) -> Table {
+    // Create the table
+    let mut table = Table::new();
+    table.add_row(row!["Links", "Status"]);
+
+    for i in 0..links.len() {
+        let string_status;
+        if status[i] {
+            string_status = "✔"
+        } else {
+            string_status = "❌"
+        }
+        table.add_row(row![links[i], string_status]);
+    }
+    table
+}
+
+
+fn get_table(link: &str, status: bool, sub_table: Table) -> Table {
+    println!("link : {:?}!", link);
+    // Create the table
+    let mut table = Table::new();
+    table.add_row(row!["Links", "Status", "Sublinks"]);
+    let string_status;
+    if status {
+        string_status = "✔"
+    } else {
+        string_status = "❌"
+    }
+    table.add_row(row![link, string_status, sub_table]);
+    table
+}
+
+// TODO use multithreading to retrieve link
+// TODO handle local links
+// TODO allow the user to specify n level of links to follow
+// TODO customize table colors / formats
